2c3ec7c26f4f743d33e66bc7ee51f6448822ed82
Z Sui <zsui2@sheffield.ac.uk>
2021-05-29 23:44:40 +0100

    vue learning

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home"
+import Lesson from "./views/Lesson"
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/lesson",
+            name: "lesson",
+            component: Lesson
+        }
+    ]
+
+})
---
+++ src/views/Lesson.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="lesson">
+        <h1>This is a test case</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
