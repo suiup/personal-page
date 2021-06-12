6eeacc341dea0328f5247bb9594060e35d4157c8
Z Sui <zsui2@sheffield.ac.uk>
2021-06-12 20:58:51 +0100

    [update] update framework

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,14 @@
-<template>
-    <div>
-        <h1>This is the header</h1>
-    </div>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-container fluid>
+        <v-toolbar
+                class="mb-2"
+                color="indigo darken-5"
+                dark
+                flat
+        >
+            <v-toolbar-title>This is a header</v-toolbar-title>
+        </v-toolbar>
+    </v-container>
 </template>
 
 <script>
