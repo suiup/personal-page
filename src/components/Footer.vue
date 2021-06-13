31a28beb8d462c4c3297b68eb3cea9ba67c0d4bf
Z Sui <zsui2@sheffield.ac.uk>
2021-06-13 02:19:54 +0100

    [update] update vuetify framework

---
+++ src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-footer
+            dark
+    >
+        <v-card
+                flat
+                tile
+                width="100%"
+                class="lighten-1 text-center"
+        >
+            <v-card-text>
+                <v-btn
+                        v-for="icon in icons"
+                        :key="icon"
+                        class="mx-4"
+                        icon
+                >
+                    <v-icon size="24px">
+                        {{ icon }}
+                    </v-icon>
+                </v-btn>
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-text class="white--text">
+                {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            icons: [
+                'mdi-home',
+                'mdi-email',
+                'mdi-calendar',
+                'mdi-delete',
+            ],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-content>
+        <v-navigation-drawer
+                permanent
+                expand-on-hover
+        >
+            <v-list>
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                        <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                    </v-list-item-avatar>
+                </v-list-item>
+
+                <v-list-item link>
+                    <v-list-item-content>
+                        <v-list-item-title class="text-h6">
+                            Sandra Adams
+                        </v-list-item-title>
+                        <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list
+                    nav
+                    dense
+            >
+                <v-list-item link>
+                    <v-list-item-icon>
+                        <v-icon>mdi-folder</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>My Files</v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-icon>
+                        <v-icon>mdi-account-multiple</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Shared with me</v-list-item-title>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-icon>
+                        <v-icon>mdi-star</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Starred</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </v-content>
+
+</template>
+
+<script>
+    export default {
+        name: "NavigationDrawer",
+        data () {
+            return {
+                drawer: true,
+                items: [
+                    { title: 'Home', icon: 'mdi-home-city' },
+                    { title: 'My Account', icon: 'mdi-account' },
+                    { title: 'Users', icon: 'mdi-account-group-outline' },
+                ],
+                mini: true,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
