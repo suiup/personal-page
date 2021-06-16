5dd8c8d05b30e31820ffa64bb81125805c18f2b4
Z Sui <zsui2@sheffield.ac.uk>
2021-06-16 23:51:41 +0100

    [update] update pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 // vue.config.js
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/suiup'
-        : '/',
-
     transpileDependencies: [
       'vuetify'
     ]
