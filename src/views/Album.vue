5068d7927255ae9f19c7514e33ac397df5f353de
Z Sui <zsui2@sheffield.ac.uk>
2021-07-12 02:35:04 +0100

    [add] add album

---
+++ src/views/Album.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container" id="app">
+        <Gallery :photos="photos"></Gallery>
+    </div>
+</template>
+
+<script>
+    import Gallery from '@/components/Gallery.vue'
+    export default {
+        components: {
+            Gallery,
+        },
+        data:function(){
+            return{
+                photos:[
+                    require("@/assets/carousels_01.jpg"),
+                    require("@/assets/carousels_02.jpg"),
+                    require("@/assets/carousels_03.jpg"),
+                    require("@/assets/carousels_04.jpg"),
+                    require("@/assets/carousels_05.jpg"),
+                    require("@/assets/carousels_06.jpg"),
+                    require("@/assets/carousels_07.jpg"),
+                    require("@/assets/carousels_08.jpg"),
+                    require("@/assets/carousels_09.jpg"),
+                ],
+                activePhoto:null
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
