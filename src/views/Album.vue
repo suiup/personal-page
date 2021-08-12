92a1bb3ae80bbd008ecbbcea3a1d2caf079ae7f5
up_up_up <45310363+suiup@users.noreply.github.com>
2021-08-13 00:41:26 +0100

    Update Album.vue
    delete

--- src/views/Album.vue
+++ src/views/Album.vue
@@ -25,7 +25,6 @@
                     require("@/assets/carousels_09.jpg"),
                     require("@/assets/7f8e1e5c3682f9a250710e00ca64ebb.jpg"),
                     require("@/assets/8f8ca8934c17e24a182e97cb9110753.jpg"),
-                    require("@/assets/a97b5907151a87720e4090826cc5b9a.jpg"),
                     require("@/assets/baa7074036de0ca15690a6c0b19671c.jpg"),
                 ],
                 activePhoto:null
