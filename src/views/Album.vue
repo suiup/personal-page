787023a22e67439ef1da67545e03684fd6d4ffb5
Z Sui <zsui2@sheffield.ac.uk>
2021-08-13 00:32:04 +0100

    [add] add some new images

--- src/views/Album.vue
+++ src/views/Album.vue
@@ -12,6 +12,7 @@
         },
         data:function(){
             return{
+                //TODO need to create a new folder and use loop to display those images
                 photos:[
                     require("@/assets/carousels_01.jpg"),
                     require("@/assets/carousels_02.jpg"),
@@ -22,6 +23,10 @@
                     require("@/assets/carousels_07.jpg"),
                     require("@/assets/carousels_08.jpg"),
                     require("@/assets/carousels_09.jpg"),
+                    require("@/assets/7f8e1e5c3682f9a250710e00ca64ebb.jpg"),
+                    require("@/assets/8f8ca8934c17e24a182e97cb9110753.jpg"),
+                    require("@/assets/a97b5907151a87720e4090826cc5b9a.jpg"),
+                    require("@/assets/baa7074036de0ca15690a6c0b19671c.jpg"),
                 ],
                 activePhoto:null
             }
